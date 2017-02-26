ea663ec73b9e4dece0efb0716513edf6da1db05a
Han Chen <chenhanleetcode@gmail.com>
2017-02-25 21:01:50 -0800

    java parser testing

--- umlparser.java
+++ umlparser.java
@@ -1,9 +1,26 @@
+import java.io.FileInputStream;
+import java.util.Optional;
+
+import com.github.javaparser.*;
+import com.github.javaparser.ast.CompilationUnit;
+import com.github.javaparser.ast.body.ClassOrInterfaceDeclaration;
+import com.github.javaparser.ast.body.FieldDeclaration;
+
 
 public class umlparser {
 
-	public static void main(String[] args) {
-		// TODO Auto-generated method stub
+	 public static void main(String[] args) throws Exception {
+	        // creates an input stream for the file to be parsed
+	        FileInputStream in = new FileInputStream("test.java");
 
-	}
+	        CompilationUnit compilationUnit = JavaParser.parse(in);
+//	        compilationUnit.getNodesByType(FieldDeclaration.class).stream().
+//	        filter(f -> f.getModifiers().contains(PRIVATE)).
+//	        forEach(f -> System.out.println("Check field at line " + f.getBegin().get().line));
+	        
+//	        Optional<ClassOrInterfaceDeclaration> classA = compilationUnit.getClassByName("A");
+	        System.out.println(compilationUnit.toString());
+	    }
 
+	   
 }
